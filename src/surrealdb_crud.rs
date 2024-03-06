7bb178c6216870847964e96b87d47faf110b14ef
ranafge <ranafge@gmail.com>
2024-03-07 00:06:53 +0600

    surrealdb crud person details added

--- src/surrealdb_crud.rs
+++ src/surrealdb_crud.rs
@@ -1,1 +1,10 @@
-use surrealdb
+use serde::{Serialize, Deserialize};
+use surrealdb::engine::remote::ws::Ws;
+use surrealdb::opt::auth::Root;
+use surrealdb::sql::Thing;
+use surrealdb::Surreal;
+
+struct Name<'a> {
+    first: &'a str,
+    last: &'a str,
+}
